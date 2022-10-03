f30aa63b1f76258498548055b97c6198699106ba
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-10-03 12:43:04 +0200

    PHP self-processing form

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="form.php" method="post">
-        <label for="name"> Name : </label>
-    <input type="text" name="name">
+<?php
 
-    </form>
-</body>
-</html>
+require __DIR__ . '/inc/header.php';
+
+$request_method = strtoupper($_SERVER['REQUEST_METHOD']);
+
+if ($request_method === 'GET') {
+	require __DIR__ . '/inc/get.php';
+} elseif ($request_method === 'POST') {
+	require __DIR__ .  '/inc/post.php';
+}
+
+require __DIR__ .  '/inc/footer.php';
